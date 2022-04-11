8394a56a70b887e2a8d40b7f458b96824b870587
ks5-dev <kienlt2306@gmail.com>
2022-04-11 12:28:55 +0700

    added a bit to syntax highlighting,run file and save as feature

--- gui.py
+++ gui.py
@@ -1,9 +1,10 @@
+from fileinput import filename
 from typing import Text
-from PyQt6.QtWidgets import (QApplication, QWidget, QPushButton, QLabel, QLineEdit, QGridLayout, QVBoxLayout, 
+from PySide6.QtWidgets import (QApplication, QWidget, QPushButton, QLabel, QLineEdit, QGridLayout, QVBoxLayout, 
 QMenu,QCompleter,QHBoxLayout, QFileDialog, QInputDialog ,QPlainTextEdit)
 import sys, os
 from pathlib import Path
-from PyQt6.QtGui import QFont, QFontDatabase, QColor, QSyntaxHighlighter, QTextCharFormat, QIcon
+from PySide6.QtGui import QFont, QFontDatabase, QColor, QSyntaxHighlighter, QTextCharFormat, QIcon
 import re
 import os
 import json
@@ -17,6 +18,14 @@ python_file_path = os.path.join(script_dir, rel_path_python)
 with open(python_file_path,"r") as f1:
     py_keys = json.load(f1)
     python_keywords = py_keys["basic_keywords"]
+
+rel_path_commands = "commands/run_commands.json"
+commands_file_path = os.path.join(script_dir, rel_path_commands)
+
+with open(commands_file_path, "r") as f2:
+    commands = json.load(f2)
+    run_py = commands["run-python"]
+    activate_venv = commands["activate-venv"]
 
 file_options = [
     {"New" : ["New File", "New Folder"]},
@@ -33,7 +42,7 @@ edit_options = [
 
 run_options = [
     "Run",
-    "Run C++"
+    "Editing commands"
 ]
 completer = [
     "nuncadamavenve"
@@ -79,7 +88,7 @@ class Window(QWidget):
 
         self.run_button = QPushButton("Run")
         self.run_menu = QMenu()
-        self.run_menu.triggered.connect(lambda x:print(x.text()))
+        self.run_menu.triggered.connect(lambda x: self.runFile(x.text()))
         self.run_button.setMenu(self.run_menu)
 
         self.curFileDisplay = QLabel("None")
@@ -96,10 +105,7 @@ class Window(QWidget):
 
         self.highlighter = Highlighter()
         self.setUpEditor()
-
-        self.completer = QCompleter(completer)
         
-
         self.mainLayout.addWidget(self.curFileDisplay,0,7)
         self.mainLayout.addWidget(self.TextBox,1,0,10,10)
         
@@ -121,19 +127,29 @@ class Window(QWidget):
         pattern = r'^\s*def\s+\w+\s*\(.*\)\s*:\s*$'
         self.highlighter.add_mapping(pattern, function_format)        
         '''
-
         
+        name_format = QTextCharFormat()
+        name_format.setForeground(QColor("#a85a32"))
+        pattern = r".*[)]\B"
+        self.highlighter.add_mapping(pattern, name_format)
+
+        parenthesis_format = QTextCharFormat()
+        parenthesis_format.setForeground(QColor("#ffffff"))
+        pattern = r'\([^()]*\)'
+        self.highlighter.add_mapping(pattern, parenthesis_format)
+
+
         keyword_format = QTextCharFormat()
-        keyword_format.setForeground(QColor("#00ad9c"))
+        keyword_format.setForeground(QColor("#32a895"))
         keyword_format.setFontWeight(350)
         #pattern = r"(?=(\b" + '\\b|\\b'.join(python_keywords) + r"\b))"
         for i in python_keywords:
-            pattern = r"".join(i)
+            pattern = fr"\b{i}\b"
             self.highlighter.add_mapping(pattern, keyword_format)
         
         # pattern 3: comment format
         comment_format = QTextCharFormat()
-        comment_format.setForeground(QColor("#77ff77"))
+        comment_format.setForeground(QColor("#32a834"))
         # pattern = r'^\s*#.*$' # hightlight from the beginning of the line
         pattern = r'#.*$' # just the text
         self.highlighter.add_mapping(pattern, comment_format)
@@ -156,12 +172,12 @@ class Window(QWidget):
 
     def fileHandle(self, command):
         if command == "Open File":
-            print(True)
             home_dir = str(Path.home())
             fname = QFileDialog.getOpenFileName(self, 'Open file', home_dir)
 
             if fname[0]:
-                self.curFileDisplay.setText(os.path.basename(fname[0]))
+                self.fileName = os.path.basename(fname[0])
+                self.curFileDisplay.setText(self.fileName)
                 self.curPath = fname[0]
                 f = open(fname[0], 'r')
 
@@ -179,8 +195,22 @@ class Window(QWidget):
             self.curFileDisplay.setText("None")
             self.TextBox.clear()
 
-print(python_keywords)
+        if command == "Save as":
+            if self.curPath != "":
+               fileSaveAs,_ = QFileDialog.getSaveFileName(self,"Save file as","",".txt;;" ".py")
+               if fileSaveAs:
+                   open(fileSaveAs+_ , "x")
+                   with open(fileSaveAs+_, "w") as writeSaveAs:
+                       writeSaveAs.write(self.TextBox.toPlainText())
+
+    def runFile(self, command):
+        if command == "Run" and self.curPath != "":
+            print("True")
+            self.curFolder = os.path.dirname(self.curPath)
+            if self.fileName.endswith(".py"):
+                os.system(run_py + " " + self.fileName)
+
 app = QApplication(sys.argv)
 window = Window()
 window.show()
-sys.exit(app.exec())
+sys.exit(app.execo())
